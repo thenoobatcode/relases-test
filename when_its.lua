d045317c9afb5c680d9edd0b0bba9a9c9cb938fe
Liagova0 <tienesunmensaje1999@gmail.com>
2026-07-22 17:28:28 -0500

    BIG change

--- when_its.lua
+++ when_its.lua
@@ -1,24 +1,27 @@
-// when he is coming, he runs //
+-- when he is coming, he runs --
+
+narrator = {}
 
 function run(he)
   print(he .. " *runs*")
+  return he .. " *runs*"
 end
 
-function scream(type) // die, normal
-  if type == "die" then
+function scream(mode) -- die, normal
+  if mode == "die" then
     return "AAAAAAAHHHHHHHGHHHRHhahhrrr----"
-  elseif type == "normal" then
+  elseif mode == "normal" then
     return "AAAAAAHHHHHHHHHHHHHH!!!!!"
   end
 end
 
 function narrator.kill(killer, with, victim)
-  print(killer .. " killed " .. victim .. " with " .. with.."!")
+  print(killer .. " killed " .. victim .. " with " .. with .. "!")
 end
 
 run("Miguel:")
-print("wacho: "..scream("normal"))
+print("wacho: " .. scream("normal"))
 print("wacho: MIGUEL ITS ATTACKING ME!")
-print("milo: RUN!!!, MIGUEL CAME BACK! ".. run("milo"))
-print("wacho: "scream("die"))
-narrator.kill("Miguel","his own fists", "Wacho")
+print("milo: RUN!!!, MIGUEL CAME BACK! " .. run("milo"))
+print("wacho: " .. scream("die"))
+narrator.kill("Miguel", "his own fists", "Wacho")
