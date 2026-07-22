2c0bf0862ab0f2abbe3773170f4660fc23f2fc91
thenoobatcode <tienesunmensaje1999@gmail.com>
2026-07-22 11:29:47 -0500

    miguel movie
    random

--- when_its.lua
+++ when_its.lua
@@ -1,1 +1,17 @@
+// when he is coming, he runs //
 
+function run(he)
+  print(he .. "ITS COMING")
+end
+
+function scream(type) // die, normal
+  if type == "die" then
+    print("AAAAAAAHHHHHHHGHHHRHhahhrrr----")
+  elseif type == "normal" then
+    print("AAAAAAAAAHHHHHHHHHH!")
+  end
+end
+
+run("Miguel")
+scream(normal)
+print("MIGUEL ITS ATTACKING ME!")
