908c1587e08def2f29329e54dae25a8d1211a46e
Liagova0 <tienesunmensaje1999@gmail.com>
2026-07-22 17:55:02 -0500

    Full Movie!

--- when_its.lua
+++ when_its.lua
@@ -3,8 +3,7 @@
 narrator = {}
 
 function run(he)
-  print(he .. " *runs*")
-  return he .. " *runs*"
+  return "(* "..he .. " runs *)"
 end
 
 function scream(mode) -- die, normal
@@ -15,13 +14,38 @@ function scream(mode) -- die, normal
   end
 end
 
-function narrator.kill(killer, with, victim)
-  print(killer .. " killed " .. victim .. " with " .. with .. "!")
+function cry(who)
+    return "(* "..who.." is crying *)"
 end
 
-run("Miguel:")
-print("wacho: " .. scream("normal"))
-print("wacho: MIGUEL ITS ATTACKING ME!")
-print("milo: RUN!!!, MIGUEL CAME BACK! " .. run("milo"))
-print("wacho: " .. scream("die"))
-narrator.kill("Miguel", "his own fists", "Wacho")
+function narrator.kill(killer, with, victim)
+  print("(* "..killer .. " killed " .. victim .. " with " .. with .. " *)")
+end
+
+print("Milo: Wacho.. i feel something with you")
+print("Milo: " .. scream("normal"))
+print("Milo: MIGUEL ITS ATTACKING ME!")
+print("Wacho: RUN!!!, MIGUEL CAME BACK! " .. run("Wacho"))
+print("Milo: " .. scream("die"))
+narrator.kill("Miguel", "his own fists", "Milo")
+print("Wacho: NOOO MILOOOO.... YOU WERE MY FRIEND "..cry("Wacho"))
+print("Wacho: i must take revenge of this")
+print("..")
+print("..")
+print("..")
+print("ten days later")
+print("Wacho: (* Recharges Glock-16 *) i must take care of this..")
+print("Miguel: (* Sleeping *)")
+print("Wacho: (* sneaks in *) ")
+print("Miguel: hmmp.. hmm!?, Who is here in MY HOUSE!.")
+print("Wacho: me.")
+print("Wacho: (* Chokes Miguel *).. SO YOU KILLED MY BEST FRIEND.")
+print("Miguel: aahuuhahaggguhua---- Ighhg-- AM--")
+print("Miguel: gwuhgug PROUD--gugwaughu OF-IT--ghuuhwhauh--")
+print("Miguel: HAHAHHAHAHAgugauwguHahhaghgaugwhhaHHAHAHHAHAHA")
+print("Wacho: YES... ARE YOU PROUD OF IT... YESSS ARE YOU SURE--")
+print("Miguel: YESguwg")
+print("Wacho: --miguel.. its over--")
+print("Wacho: (* Fires gun *)")
+narrator.kill("Wacho","a Glock-16","Miguel")
+print("Wacho: -its over. miguel-")
